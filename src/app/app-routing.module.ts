5506ef96e83c0b73b1051326834cb2d95a2d841a
Anton Drofenik <anton.drofenik@agiledrop.com>
2023-03-30 17:29:26 +0000

    Initial module setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./modules/products/products.module').then(m => m.ProductsModule),
+  },
+  {
+    path: 'users',
+    loadChildren: () =>
+      import('./modules/users/users.module').then(m => m.UsersModule),
+  },
+  {
+    path: 'carts',
+    loadChildren: () =>
+      import('./modules/carts/carts.module').then(m => m.CartsModule),
+  },
+  { path: '**', redirectTo: 'products' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/modules/carts/carts.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carts',
+  templateUrl: './carts.component.html',
+  styleUrls: ['./carts.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CartsComponent {
+
+}
