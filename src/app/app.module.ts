c962e79ae7668f74187f2aed8d819dbffeb0a4ec
Anton Drofenik <anton.drofenik@agiledrop.com>
2023-03-31 17:26:11 +0000

    Created shared folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ApiInterceptor } from './interceptors/api.interceptor';
+import { ApiInterceptor } from './shared/interceptors/api.interceptor';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
