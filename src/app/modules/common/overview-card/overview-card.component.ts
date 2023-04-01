88be4374e581179fcf44e421eba134baf4c0107b
Anton Drofenik <anton.drofenik@agiledrop.com>
2023-04-01 15:31:15 +0000

    Added link to overview card

--- src/app/modules/common/overview-card/overview-card.component.ts
+++ src/app/modules/common/overview-card/overview-card.component.ts
@@ -1,16 +1,19 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-overview-card',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, RouterModule, MatCardModule, MatButtonModule],
   templateUrl: './overview-card.component.html',
   styleUrls: ['./overview-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OverviewCardComponent {
+  @Input() link!: string[];
   @Input() title!: string;
   @Input() subtitles!: string[];
   @Input() imageSrc!: string;
--- src/app/modules/users/users.component.ts
+++ src/app/modules/users/users.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { User } from 'src/app/types';
 import { UsersService } from './services/users.service';
 import { OverviewBaseComponent } from 'src/app/shared/components/overview-base.component';
+import { ApiEndpoints } from 'src/app/shared/models/ApiEndpoints.model';
 
 @Component({
   selector: 'app-users',
@@ -10,6 +11,8 @@ import { OverviewBaseComponent } from 'src/app/shared/components/overview-base.c
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UsersComponent extends OverviewBaseComponent<User> {
+  public readonly usersEndpoint = ApiEndpoints.USERS;
+
   constructor(protected override service: UsersService) {
     super(service);
   }
